4e881a231162de74428e8e34b2bf4b1756076834
Rodrigo Cardoso <rodrigo.cardoso1007@gmail.com>
2024-01-23 09:16:54 -0300

    Cores do projeto

---
+++ src/styles/colors.ts
@@ -0,0 +1,12 @@
+const colors = {
+  primary: {
+    light: "#DAF2E4",
+    defult: "#2B7A4B",
+    dark: " #1A5331"
+  },
+  background: {
+    light: "#DAF2E4",
+    defult: "#F5FAF7",
+    dark: "#32B768",
+  }
+}
